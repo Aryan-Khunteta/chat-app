b228f433c114312b3c652adb3dcab10771ba7a87
Aryan Khunteta <aryankhunteta@gmail.com>
2024-09-26 01:46:13 +0530

    new

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://chat_app_admin:admin1234@cluster0.helnt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.helnt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.connect(url, {
     useNewUrlParser: true,
